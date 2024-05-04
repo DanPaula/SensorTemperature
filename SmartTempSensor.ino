2521ba3582466cd0e7cce96e656fdbc40489f64f
Dan Paula <paula35799@gmail.com>
2024-05-04 18:59:57 +0300

    send sensor data to firebase

--- SmartTempSensor.ino
+++ SmartTempSensor.ino
@@ -1,144 +1,132 @@
 #include <ESP8266WiFi.h>
-#include <DNSServer.h>
-#include <ESP8266WebServer.h>
-#include <WiFiManager.h>
 #include <DHT.h>
-#include <ESP8266HTTPClient.h>
+#include <ESP8266WebServer.h>
+#include <Firebase_ESP_Client.h>
+//Provide the token generation process info.
+#include "addons/TokenHelper.h"
+//Provide the RTDB payload printing info and other helper functions.
+#include "addons/RTDBHelper.h"
 
+#define DHTPIN D7
+#define DHTTYPE DHT11
+
+const char* ssid = "";
+const char* password = "";
 #define FIREBASE_AUTH ""
 #define FIREBASE_HOST ""
-
-#define TRIGGER_PIN D2
-
-#define DHTTYPE DHT11   // DHT 11
-
-#define WIFI_SSID ""
-#define WIFI_PASSWORD ""
-// DHT Sensor
-uint8_t DHTPin = D7; 
-// Initialize DHT sensor.
-DHT dht(DHTPin, DHTTYPE);     
+#define USER_EMAIL ""
+#define USER_PASSWORD ""
 
 ESP8266WebServer server(80);
+DHT dht(DHTPIN, DHTTYPE);
 
-float Temperature;
-float Humidity;
-
-const char* firebaseHost = "";
-const char* firebasePath = "";
-
-String tempPath = "/temperature";
-String humPath = "/humidity";
 String userId;
 String sensorId = "3535ras";
+//Define Firebase Data object
+FirebaseData fbdo;
 
-WiFiClient client;
-String uid;
-String databasePath; //saves database main path, which will be updated to user UID 
-unsigned long sendDataPrevMillis = 0;
+FirebaseAuth auth;
+FirebaseConfig config;
+//unsigned long sendDataPrevMillis = 0;
+//int count = 0;
 bool signupOK = false;
 
-// Initialize WiFi
-void initWiFi() {
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  Serial.print("Connecting to WiFi ..");
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print('.');
-    delay(1000);
-  }
-  Serial.println(WiFi.localIP());
-  Serial.println();
-}
-
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
-  Serial.println("\n Starting...");
+  Serial.println();
 
-  initWiFi();
-  pinMode(DHTPin, INPUT);
-  dht.begin();  
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to ");
+  Serial.print(ssid);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
 
- // Start HTTP server
+  // Start HTTP server
   server.on("/user", HTTP_POST, handleUserData);
-  server.on("/sensor", handleSensorData);
   server.begin();
-  Serial.println("HTTP server started");  
+  Serial.println("HTTP server started");
+
+  /* Assign the api key (required) */
+  config.api_key = FIREBASE_AUTH;
+
+  /* Assign the RTDB URL (required) */
+  config.database_url = FIREBASE_HOST;
+
+  // Assign the user sign in credentials
+  auth.user.email = USER_EMAIL;
+  auth.user.password = USER_PASSWORD;
+
+  Firebase.reconnectWiFi(true);
+  fbdo.setResponseSize(4096);
+
+
+  // Assign the callback function for the long running token generation task
+  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
+
+  // Assign the maximum retry of token generation
+  config.max_token_generation_retry = 5;
+
+  // Initialize the library with the Firebase authen and config
+  Firebase.begin(&config, &auth);
+  
+  Serial.println("new way");
+  // Initialize DHT sensor
+  dht.begin();
 }
 
 void loop() {
-  
-  float h = dht.readHumidity();                               
-  float t = dht.readTemperature();
 
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
   server.handleClient();
+  String path = "/Users/" + userId + "/Sensors/" + sensorId;
 
-  if (isnan(h) || isnan(t))                                     // Checking sensor working
-  {                                   
-    Serial.println(F("Failed to read from DHT sensor!"));
-    return;
-  } 
-  Serial.print("Humidity: ");  
-  Serial.print(h);
-  
-  Serial.print("%  Temperature: ");  
-  Serial.print(t);  
-  Serial.println("°C ");
-  delay(5000);
-
-  if (WiFi.status() == WL_CONNECTED) {
-    HTTPClient http;
-    WiFiClient client; // Create WiFiClient object
-
-    String firebasePathTemperature = "/Users" + userId + "/Sensors" + sensorId + "/temperature";
-    String firebasePathHumidity = "/Users" + userId + "/Sensors" + sensorId + "/humidity";
-    // Construct Firebase URL
-    String urlTemperature = "https://" + String(firebaseHost) + String(firebasePathTemperature);
-    String urlHumidity = "https://" + String(firebaseHost) + String(firebasePathHumidity);
-
-    // Send POST request with sensor data
-    http.begin(client, urlTemperature);
-    http.addHeader("Content-Type", "application/json");
-    int httpResponseCodeT = http.POST(String(t));
-
-    if (httpResponseCodeT > 0) {
-      Serial.print("HTTP Response code: ");
-      Serial.println(httpResponseCodeT);
-    } else {
-      Serial.print("Error in HTTP request: ");
-      Serial.println(httpResponseCodeT);
+  if (Firebase.ready()) {
+    Serial.println(userId.length());
+    if (!isnan(humidity) && !isnan(temperature) && userId.length() > 0) {
+      Serial.println("first if");
+      if (Firebase.RTDB.setFloat(&fbdo, path + "/humidity",humidity)){
+       Serial.print("Humidity: ");
+       Serial.println(humidity);
+      
+    }
+    else {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + fbdo.errorReason());
+    }
+    
+    
+    // Write an Float number on the database path test/float
+    if (Firebase.RTDB.setFloat(&fbdo, path + "/temperature", temperature)){
+       Serial.print("Temperature: ");
+       Serial.println(temperature);
+    }
+    else {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + fbdo.errorReason());
     }
 
-    http.begin(client, urlHumidity);
-    http.addHeader("Content-Type", "application/json");
-    int httpResponseCodeH = http.POST(String(h));
-
-    if (httpResponseCodeH > 0) {
-      Serial.print("HTTP Response code: ");
-      Serial.println(httpResponseCodeH);
-    } else {
-      Serial.print("Error in HTTP request: ");
-      Serial.println(httpResponseCodeH);
     }
-
-
-    http.end();
   }
 
-  delay(5000); // Send data every 5 seconds
+  
+
+  delay(5000); // Adjust as needed
 }
 
 void handleUserData() {
-  userId = server.arg("user");
-  Serial.println("Received user ID: " + userId);
-  server.send(200, "text/plain", "User ID received");
-}
-
-void handleSensorData() {
-  if (userId.length() > 0) {
-    Serial.println("Sending sensor ID: " + sensorId);
-    server.send(200, "text/plain", sensorId);
+  if (server.hasArg("userId")) {
+    userId = server.arg("userId");
+    server.send(200, "text/plain", "User ID received");
   } else {
-    server.send(400, "text/plain", "User ID not received");
+    server.send(400, "text/plain", "Missing user ID");
   }
 }
